2a20b656ed65cb50be3bc390c6950711ae991789
Galuh <galuhhakim97@gmail.com>
2022-06-12 12:39:12 +0700

    advance callback

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require('fs');
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,28 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  const result = [];
+  fs.readFile(file1, 'utf8', function read(err, data) {
+    if (err) fnCallback(err,null);
+    let parsedData = JSON.parse(data)
+    message = parsedData.message;
+    result.push(message.split(' ')[1])
+    fs.readFile(file2, 'utf8', function read(err, data) {
+      if (err) fnCallback(err,null);
+      parsedData = JSON.parse(data)
+      message = parsedData[0].message;
+      result.push(message.split(' ')[1])
+      fs.readFile(file3, 'utf8', function read(err, data) {
+        if (err) fnCallback(err,null);
+        parsedData = JSON.parse(data)
+        message = parsedData[0].data.message;
+        result.push(message.split(' ')[1])
+        fnCallback(null,result);
+      })
+    })
+  })
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
